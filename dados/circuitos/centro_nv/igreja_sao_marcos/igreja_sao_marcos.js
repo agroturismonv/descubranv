c7e227d9c509a266e4c8274daf96776692858c33
agroturismonv <agroturismonv@gmail.com>
2026-04-24 06:13:07 -0300

    Update igreja_sao_marcos.js

--- dados/circuitos/centro_nv/igreja_sao_marcos/igreja_sao_marcos.js
+++ dados/circuitos/centro_nv/igreja_sao_marcos/igreja_sao_marcos.js
@@ -14,18 +14,18 @@ window.LOCAL_IGREJA_SAO_MARCOS = Object.freeze({
   texts: {
     pt: {
       title: "Igreja de São Marcos",
-      subtitle: "Lugar de Fé e Espiritualidade",
-      description: "A Paróquia São Marcos em Nova Venécia-ES, com sede no \"Morro da Matriz\", teve sua construção iniciada em 1961 e finalizada em 1965, organizada pelos missionários combonianos e pelo sr. Milson da Luz. A devoção foi trazida por imigrantes italianos, com raízes em uma capela anterior, consolidando-se como pilar religioso e histórico da cidade. "
+      subtitle: "Lugar de fé e espiritualidade",
+      description: "A Paróquia São Marcos em Nova Venécia-ES, com sede no \"Morro da Matriz\", teve sua construção iniciada em 1961 e finalizada em 1965, organizada pelos missionários combonianos e pelo sr. Milson da Luz. A devoção foi trazida por imigrantes italianos, com raízes em uma capela anterior, consolidando-se como pilar religioso e histórico da cidade."
     },
     en: {
       title: "St. Mark's Church",
-      subtitle: "Place of Faith and Spirituality",
-      description: "The São Marcos Parish in Nova Venécia-ES, headquartered in \"Morro da Matriz\", had its construction started in 1961 and completed in 1965, organized by the Comboni missionaries and Mr. Milson da Luz. The devotion was brought by Italian immigrants, with roots in a previous chapel, consolidating itself as a religious and historical pillar of the city."
+      subtitle: "Place of faith and spirituality",
+      description: "The São Marcos Parish in Nova Venécia-ES, headquartered in \"Morro da Matriz\", had its construction started in 1961 and completed in 1965, organized by the Comboni missionaries and Mr. Milson da Luz."
     },
     es: {
       title: "Iglesia de San Marcos",
-      subtitle: "Lugar de Fe y Espiritualidad",
-      description: "La Parroquia de São Marcos en Nova Venécia-ES, con sede en \"Morro da Matriz\", tuvo su construcción iniciada en 1961 y terminada en 1965, organizada por los misioneros combonianos y el Sr. Milson da Luz. La devoción fue traída por inmigrantes italianos, con raíces en una capilla anterior, consolidándose como un pilar religioso e histórico de la ciudad."
+      subtitle: "Lugar de fe y espiritualidad",
+      description: "La Parroquia de São Marcos en Nova Venécia-ES tuvo su construcción iniciada en 1961 y terminada en 1965, consolidándose como un pilar religioso e histórico."
     }
   },
   RAvisionScreen: true,
