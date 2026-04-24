ebfd86a6bfade92012243688bb03c660907fa397
agroturismonv <agroturismonv@gmail.com>
2026-04-24 12:50:29 -0300

    Update config.js

--- dados/circuitos/centro_nv/config.js
+++ dados/circuitos/centro_nv/config.js
@@ -1,4 +1,4 @@
-window.CONFIG_CENTRO_NV = Object.freeze({
+/*window.CONFIG_CENTRO_NV = Object.freeze({
   id: "centro_nv",
   cover: "dados/circuitos/centro_nv/images/sao_marcos2.jpg",
   banner: "dados/circuitos/centro_nv/images/sao_marcos2.jpg",
@@ -16,6 +16,18 @@ window.CONFIG_CENTRO_NV = Object.freeze({
     subtitle: "Donde todo comenzó"
   }},
   locais: ['igreja_sao_marcos', 'casa_de_pedra_do_perletti']
-});
+});*/
+
+
+{
+  "id": "centro_nv",
+  "cover": "dados/circuitos/centro_nv/imagens/capa.jpg",
+  "texts": {
+    "pt": {
+      "title": "Centro de Nova Venécia",
+      "subtitle": "Região central"
+    }
+  }
+}
 
 
